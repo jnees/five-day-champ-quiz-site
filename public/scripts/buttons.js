36727d447af55d04300d0c6b9bbb25633488b913
jnees <joshua.nees@gmail.com>
2020-02-25 23:50:38 -0800

    Update UI

---
+++ public/scripts/buttons.js
@@ -0,0 +1,8 @@
+function showQuestion() {
+  var x = document.getElementById("question");
+  if (x.style.display === "none") {
+    x.style.display = "inline-block";
+  } else {
+    x.style.display = "none";
+  }
+}
